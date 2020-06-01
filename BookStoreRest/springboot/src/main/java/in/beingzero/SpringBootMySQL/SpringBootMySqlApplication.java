f09c084d2094218e329dfc81fd3bcb099bfd2a51
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2020-06-01 14:39:35 +0530

    Bundling a Hello World SpringBoot App

---
+++ BookStoreRest/springboot/src/main/java/in/beingzero/SpringBootMySQL/SpringBootMySqlApplication.java
@@ -0,0 +1,14 @@
+package in.beingzero.SpringBootMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMySqlApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(SpringBootMySqlApplication.class, args);
+	}
+
+}
